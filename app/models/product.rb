a356ecdff53fc82f3817283b18bacc1f431ff857
Eliot <eliotpitman@gmail.com>
2022-03-18 12:21:39 -0500

    adds image table

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,18 @@
 class Product < ApplicationRecord
-
+  
   validates :name, presence: true
   validates :supplier_id, presence: true
 
-  def supplier
-    Supplier.find_by(id: supplier_id)
-  end
+ 
+  has_many :images
+  # def images
+  #   Image.where(product_id: id)
+  # end
+
+  belongs_to :supplier
+  # def supplier
+  #   Supplier.find_by(id: supplier_id)
+  # end
   
   def is_discounted?
     if price.to_i < 10 
@@ -16,10 +23,10 @@ class Product < ApplicationRecord
   end
 
   def tax
-    price.to_i * 0.09
+    (price.to_i * 0.09).round(2)
   end
-  
+
   def total
-    price.to_i + tax
+    (price.to_i + tax).round(2)
   end
 end
